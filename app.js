11e2dab2b05bf71e6dd60e7a6942882bb7b4dca3
Mercymaryantony <mercyantony1112@gmail.com>
2024-06-13 23:01:46 +0530

    login api done

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const mongoose =  require("mongoose")
 const cors =  require("cors")
 const app = express()
 const bcrypt =require("bcryptjs")
+const jwt =require("jsonwebtoken")
 app.use(cors())
 app.use(express.json())
+
+mongoose.connect("mongodb+srv://mercy1112:mercy1112@cluster0.8x8j3ya.mongodb.net/ksrtcuserDB?retryWrites=true&w=majority&appName=Cluster0")
+
 
 const {usermodel}=require("./models/user")
 
@@ -12,6 +16,32 @@ const generatepswd = async(pswd)=>{
     const salt =await bcrypt.genSalt(4)
     return bcrypt.hash(pswd,salt)
 }
+
+app.post("/login",(req,res)=>{
+    let input=req.body
+    usermodel.find({"emailid":req.body.email}).then(
+        (response)=>{
+            if(response.length>0){
+                let dbpass = response[0].pass
+                console.log(dbpass)
+                bcrypt.compare(input.pass,dbpass,(error,isMatch)=>{
+                    if (isMatch) {
+                        jwt.sign({email:input.emailid},"blog-app",{expiresIn:"1d"},
+                            (error,token)=>{
+                            if (error) {
+                                res.json({"status":"unable to create token"})
+                            } else {
+                                res.json({"status":"success","userid":response[0]._id,"token":token})
+                            }
+                        })
+                    } else {
+                        res.json({"status":"incorrect password"})
+                    }
+                })
+            }
+        }
+    )
+})
 
 app.post("/signup",async(req,res)=>{
     let input = req.body
